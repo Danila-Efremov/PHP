c10de946636fb785a7001867fa8d210e66028286
Danila-Efremov <Efremov573@yandex.ru>
2025-12-19 00:43:42 +0300

    Add files via upload

---
+++ lab8/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'f1176559_bd');
+define('DB_USER', 'f1176559_zz');
+define('DB_PASSWORD', '_f1176559_bd_');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+?>
