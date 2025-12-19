28ee8010b5cc22f50828370bb28c74cbc00774cd
Danila-Efremov <Efremov573@yandex.ru>
2025-12-19 15:30:59 +0300

    Delete lab8/config-sample.php

--- lab8/config-sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config.sample.php (этот файл будет в GitHub)
-define('DB_NAME', 'database_name_here');
-define('DB_USER', 'username_here');
-define('DB_PASSWORD', 'password_here');
-define('DB_HOST', 'localhost');
-define('DB_CHARSET', 'utf8');
-?>
