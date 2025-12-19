19a9f6c0d754d93ecac5a8e2f3fc745abe965d3e
Danila-Efremov <Efremov573@yandex.ru>
2025-12-19 15:30:16 +0300

    Update config.php

--- lab8/config.php
+++ lab8/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_NAME', '11');
-define('DB_USER', '2');
-define('DB_PASSWORD', '1');
+define('DB_NAME', '');
+define('DB_USER', '');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 ?>
